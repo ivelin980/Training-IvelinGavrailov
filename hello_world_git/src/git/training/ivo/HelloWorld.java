e4e22ef98ce0a3c7c1f8e30614333c64849b29f5
NIcks0n <NIcks0n@NIcks0n-PC>
2020-05-22 23:50:58 +0300

    Create hello_world_git project

---
+++ hello_world_git/src/git/training/ivo/HelloWorld.java
@@ -0,0 +1,13 @@
+package git.training.ivo;
+
+/**
+ * @author Nikolay Gavrailov
+ * @since 22.05.2020
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO: Print "Hello world in console"
+	}
+}
