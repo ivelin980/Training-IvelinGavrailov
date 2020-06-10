522278ec0eb09bb176b09a8c63ba8c06a630a0e7
Nikolay.Gavrailov <niksan910@gmail.com>
2020-06-10 10:57:34 +0300

    #6 Example of class comment

--- javaFundamentals_basicSyntax/src/javaFundamentals_basicSyntax/BackIn30Minutes.java
+++ javaFundamentals_basicSyntax/src/javaFundamentals_basicSyntax/BackIn30Minutes.java
@@ -2,6 +2,23 @@ package javaFundamentals_basicSyntax;
 
 import java.util.Scanner;
 
+/**
+ * Every time Stamat tries to pay his bills he sees on the cash desk the sign:
+ * &quot;I will be back in 30 minutes&quot;. One day Stamat was sick of waiting
+ * and decided he needs a program, which prints the time after 30 minutes. That
+ * way he won’t have to wait on the desk and come at the appropriate time. He
+ * gave the assignment to you, so you have to do it.
+ * <p>
+ * Input The input will be on two lines. On the first line, you will receive the
+ * hours and on the second you will receive the minutes.
+ * <p>
+ * Output Print on the console the time after 30 minutes. The result should be
+ * in format &quot;hh:mm&quot;. The hours have one or two numbers and the
+ * minutes have always two numbers (with leading zero).
+ * 
+ * @author Ivelin.Gavrailov
+ * @since 09.06.2020
+ */
 public class BackIn30Minutes {
 
 	public static void main(String[] args) {
